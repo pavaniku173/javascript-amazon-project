c324634f456abf980dde849136972aecc32a70ee
pavani <pavani.ku173@gmail.com>
2025-08-14 17:00:45 -0400

    used modules,import and export

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,4 @@
+import {cart} from '../data/cart.js';
 /*const products =[{
     image:'images/products/athletic-cotton-socks-6-pairs.jpg',
     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
