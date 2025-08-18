14a0e8f73ee440881dff3fc0e0d67e29a08b1643
pavani <pavani.ku173@gmail.com>
2025-08-18 17:41:08 -0400

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,8 @@ let cartHTML='';  //variable to hold cart HTML
     console.log(matchingProduct);
 cartHTML+=  //append HTML for each cart item
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -108,6 +109,9 @@ cartHTML+=  //append HTML for each cart item
        console.log(prodId);  //log product id to console
        removeFromCart(prodId);  //call function to remove product from cart
        console.log(cart);
+       const conatiner=document.querySelector(`.js-cart-item-container-${prodId}`);
+       console.log(conatiner);
+       conatiner.remove();
     })
 
  });
